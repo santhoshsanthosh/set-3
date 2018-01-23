64093bb1f24bf3c29aac86b1718df13c58a3609a
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-01-23 10:24:54 +0000

    Update format subtract.c

--- format subtract.c
+++ format subtract.c
@@ -2,10 +2,10 @@
 int main()
 {
   int h1,m1,h2,m2,h3,m3;
-	scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
-  h3=h1-h2;
-	m3=m1-m2;
-	printf(" %d\t%d",h3,m3);
-	return 0;
+   scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
+   h3=h1-h2;
+   m3=m1-m2;
+   printf(" %d\t%d",h3,m3);
+   return 0;
 }
 
